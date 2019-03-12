9eadba1ab77ffa338748058177399e19703af1d9
John Lima <john.limadeveloper@gmail.com>
2019-03-11 23:53:27 -0300

    Setup source files and demo project

---
+++ Demo/BTDemo/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  BTDemo
+//
+//  Created by John Lima on 11/03/19.
+//  Copyright © 2019 limadeveloper. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+}
---
+++ Demo/BTDemo/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  BTDemo
+//
+//  Created by John Lima on 11/03/19.
+//  Copyright © 2019 limadeveloper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
